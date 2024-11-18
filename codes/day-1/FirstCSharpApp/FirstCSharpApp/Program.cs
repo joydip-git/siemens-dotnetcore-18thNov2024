19ed421c4abcefca216aafddf8d0bf1d9c28e400
Joydip <joydip.mondal@gmail.com>
2024-11-18 12:59:23 +0530

    codes, notes abd diagrans uploaded

---
+++ codes/day-1/FirstCSharpApp/FirstCSharpApp/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Hello, World!");
+
+int x = 10;
+//Person person1 = new Person(1, "joydip", 1000,"siemens");
+//Person person2 = new Person(2, "anil", 2000, "siemens");
+//person1.GetInformation();
+Person.GetInformation();
+
+
+Call();
+//local function
+void Call()
+{
+    Console.WriteLine("hello world...");
+    Console.ReadLine();
+}
+class Person
+{
+    private int id;
+    private string name = "";
+    private decimal salary;
+    static string companyName="siemens";
+
+    public Person()
+    {
+
+    }
+    public Person(int id, string name, decimal salary)
+    {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public static string GetInformation()
+    {
+        //return $"{this.id},{this.name},{this.salary}";
+        return "";
+    }
+}
---
+++ codes/day-1/FirstProject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MyApp
+{
+    public class Program
+    {
+        //public static int Main(string[] args)
+        //public static void Main()
+        //public static int Main()
+        public static void Main()
+        {
+            // Program p = new();
+            // p.Call();
+            Call();
+            Type
+        }
+        static void Call()
+        {
+            Console.WriteLine("hello world...");
+            Console.ReadLine();
+        }
+
+    }
+}
+
+//top level statement
+//Console.WriteLine("hello world");
