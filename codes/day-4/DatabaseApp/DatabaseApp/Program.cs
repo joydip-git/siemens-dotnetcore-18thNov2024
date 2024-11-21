5be90b1fe3bbcfff79a803576932ee63f1ae393d
Joydip <joydip.mondal@gmail.com>
2024-11-21 11:18:10 +0530

    code update

--- codes/day-4/DatabaseApp/DatabaseApp/Program.cs
+++ codes/day-4/DatabaseApp/DatabaseApp/Program.cs
@@ -1,7 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
 
 FetchData();
 static void FetchData()
 {
+    //using sql server authentication connecting the server in the local machine
+    SqlConnection connection = new SqlConnection(@"server=.\sqlexpress;user id=sa;password=;TrustServerCertificate=true");
 
+    //using sql server authentication connecting the server in docker container
+    //SqlConnection connection = new SqlConnection(@"server=localhost\sqlexpress,1433;user id=sa;password=;TrustServerCertificate=true");
+
+    connection.Open();
+    Console.WriteLine(connection.State.ToString());
+    connection.Close();
 }
--- codes/day-4/DatabaseApp/DatabaseApp/obj/Debug/net8.0/DatabaseApp.AssemblyInfo.cs
+++ codes/day-4/DatabaseApp/DatabaseApp/obj/Debug/net8.0/DatabaseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3668c14f8523aa3c0fd297efff45ebd8ae3b3cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bed1872dc82b266db71a98b5a5056b56165d7c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
